c53309d25c95a7b33974eef8c03d762ff0a67202
Adilem1 <adilem1598@hotmail.com>
2024-12-10 22:09:30 -0500

    pdf2

--- src/app/core/pdf/pdf-display/pdf-display.component.ts
+++ src/app/core/pdf/pdf-display/pdf-display.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pdf-display',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './pdf-display.component.scss'
 })
 export class PdfDisplayComponent {
+  pdfTitle: string = 'Geografía universal';
+  pdfUrl: string = '/assets/Geografía universal .pdf'; // Ruta del PDF en tu proyecto
 
+  constructor(public sanitizer: DomSanitizer) {}
+
+  openComments() {
+    // Acción al hacer clic en el botón de comentarios
+    alert('Sección de comentarios abierta');
+  }
 }

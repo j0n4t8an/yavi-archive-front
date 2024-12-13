8498f7a1ebc93ee786ec1619d4616ad974f746bc
j0n4t8an <morajonathanjm24@gmail.com>
2024-12-12 22:43:17 -0500

    metodo token otra vez

--- src/app/core/library/library-list/library-list.component.ts
+++ src/app/core/library/library-list/library-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../../services/books-http.service';
+import * as jwt_decode from 'jwt-decode';
+
 
 @Component({
   selector: 'app-library-list',
@@ -10,6 +12,7 @@ export class LibraryListComponent implements OnInit {
   books: any[] = []; // Lista de libros
   dialogVisible: boolean = false;
   selectedBook: any = null;
+  decodedToken: any={};
 
   constructor(private bookService: BookService) {}
 
@@ -30,4 +33,29 @@ export class LibraryListComponent implements OnInit {
     // Lógica para descargar el libro (puedes implementarlo según tu caso)
     alert(`Descargando el libro: ${book.title}`);
   }
+
+
+getToken() {
+  const token = sessionStorage.getItem('token');
+
+  if (token) {
+    try {
+      // Decodificar el token
+      this.decodedToken = jwt_decode.jwtDecode(token);
+      console.log('Decoded Token:', this.decodedToken);
+      if (this.decodedToken) {
+        console.log('User ID:', this.decodedToken.id || 'No ID in token');
+        console.log('User Email:', this.decodedToken.email || 'No Email in token');
+        console.log('User Name:', this.decodedToken.firstName || 'No Name in token');
+      } else {
+        console.log('Decoded token is null or undefined');
+      }
+    } catch (error) {
+      console.error('Error decoding token:', error);
+    }
+  } else {
+    console.log('No token found in sessionStorage');
+  }
+}
+
 }
